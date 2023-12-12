ca64d7c915e4c901fd89fcb5cab1ebf36691846c
MohileshM <123352684+MohileshM@users.noreply.github.com>
2023-12-12 23:49:15 +0530

    Add files via upload

---
+++ src/Slices/HomeSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    value: null
+}
+export const HomeSlice = createSlice({
+    name: 'Index',
+    initialState,
+    reducers: {
+        IndexHandler: (state,action) =>{
+            state.value = action.payload
+            console.log(state.value)
+            
+
+        }
+    }
+})
+
+export const {IndexHandler} = HomeSlice.actions;
+export const Ind = HomeSlice.reducer;
